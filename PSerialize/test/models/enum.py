4c9fc46e4bc3ff277f9e2385e61edca25fcefe85
Steffen Cucos <steffen.cucos@gmail.com>
2022-06-28 11:21:56 -0400

    Enum serialization test

---
+++ PSerialize/test/models/enum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Number(Enum):
+    ONE = "one"
+    TWO = "two"
+    THREE = "three"
+    FOUR = "four"
+    FIVE = "five"
+    SIX = "six"
---
+++ PSerialize/test/tests/enum_test.py
@@ -0,0 +1,26 @@
+
+import pytest
+
+
+from  ...src.serialize import serialize
+from  ...src.deserialize import deserialize
+
+from ..models.enum import Number
+
+
+@pytest.mark.parametrize("input,expected", [
+    (Number.ONE, "one"),
+    ([Number.ONE, Number.TWO], ["one", "two"]),
+    ({Number.FIVE: 5}, {"five": 5})
+])
+def test_serialize_enum(input, expected):
+    assert serialize(input) == expected
+
+@pytest.mark.parametrize("input,type,expected", [
+    ("one", Number, Number.ONE),
+    (["one", "two"], list[Number], [Number.ONE, Number.TWO]),
+    ({"five": 5}, dict[Number,int], {Number.FIVE: 5})
+])
+def test_deserialize_enum(input, type, expected):
+    assert deserialize(input, type) == expected
+
