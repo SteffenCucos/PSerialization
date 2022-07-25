db163becd82dfc01ba71cea04d15ea6a46c7b26e
Steffen Cucos <steffen.cucos@gmail.com>
2022-07-25 11:08:57 -0400

    Make serialize a class, add optional middleware

--- PSerialize/src/serialize.py
+++ PSerialize/src/serialize.py
@@ -1,41 +1,50 @@
 
-from typing import Any
+from typing import Any, Callable
 
 from .serialization_utils import (
     is_primitive,
     is_enum
 )
 
-def serialize_object(object: object) -> dict:
-    return serialize_dict(vars(object))
+class Serializer:
+    def __init__(self, middleware: dict[type, Callable[[object], type]] = []):
+        self.middleware = middleware
 
-def serialize_dict(dict: dict) -> dict:
-    serializedDict = {}
-    for key, value in dict.items():
-        serializedKey = serialize(key)
-        serializedValue = serialize(value)
-        serializedDict[serializedKey] = serializedValue
+    def serialize_basic_object(self, object: object) -> dict:
+        return self.serialize_dict(vars(object))
 
-    return serializedDict
+    def serialize_dict(self, dict: dict) -> dict:
+        serializedDict = {}
+        for key, value in dict.items():
+            serializedKey = self.serialize(key)
+            serializedValue = self.serialize(value)
+            serializedDict[serializedKey] = serializedValue
 
-def serialize_list(list: list) -> list:
-    serializedList = []
-    for element in list:
-        serializedList.append(serialize(element))
-    
-    return serializedList
+        return serializedDict
 
-def serialize(value: Any):
-    if value == None:
-        return None
-    classType = type(value)
-    if is_primitive(classType):
-        return value
-    if is_enum(classType):
-        # Represent enums as their String representation
-        return serialize(value.value)
-    if classType is type([]):
-        return serialize_list(value)
-    if classType is type({}):
-        return serialize_dict(value)
-    return serialize_object(value)
+    def serialize_list(self, list: list) -> list:
+        serializedList = []
+        for element in list:
+            serializedList.append(self.serialize(element))
+        
+        return serializedList
+
+    def serialize(self, value: Any):
+        if value == None:
+            return None
+        classType = type(value)
+        if is_primitive(classType):
+            return value
+        if is_enum(classType):
+            # Represent enums as their String representation
+            return self.serialize(value.value)
+        if classType is type([]):
+            return self.serialize_list(value)
+        if classType is type({}):
+            return self.serialize_dict(value)
+        if (serializer := self.middleware.get(classType, None)) != None:
+            return serializer(value)
+
+        return self.serialize_basic_object(value)
+
+default_serializer = Serializer(middleware={})
